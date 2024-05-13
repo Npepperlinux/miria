0b934ffdcea3c06dbf526f09a9c0558c9acd9e2b
Npepperlinux <alterlinuxer@gmail.com>
2024-05-13 11:53:07 +0900

    Revert "ShareExtensionから共有したものを読み出せるところまで"
    This reverts commit 63bea8b8415801c59228e4b0da3e14c9f6aa7fc4.

--- lib/view/share_extension_page/share_extension_page.dart
+++ lib/view/share_extension_page/share_extension_page.dart
@@ -4,49 +4,32 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preference_app_group/shared_preference_app_group.dart';
 
 @RoutePage()
-class ShareExtensionPage extends ConsumerStatefulWidget {
+class ShareExtensionPage extends ConsumerWidget {
   const ShareExtensionPage({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      ShareExtensionPageState();
-}
-
-class ShareExtensionPageState extends ConsumerState<ShareExtensionPage> {
-  var sharedPreference = "";
-  var sharedData = "";
-  String? error;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    Future(() async {
-      try {
-        await SharedPreferenceAppGroup.setAppGroup(
-            "group.info.shiosyakeyakini.miria");
-        sharedPreference =
-            await SharedPreferenceAppGroup.get("account_settings") as String? ??
-                "";
-        sharedData =
-            await SharedPreferenceAppGroup.get("ShareKey") as String? ?? "";
-        await SharedPreferenceAppGroup.setString("ShareKey", "");
-        setState(() {});
-      } catch (e) {
-        setState(() {
-          error = e.toString();
-        });
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Share Extension Page Test"),
       ),
-      body: Text(error ??
-          "SharedPreference: $sharedPreference\n共有されたデータ: $sharedData"),
+      body: FutureBuilder(
+        future: Future(() async {
+          await SharedPreferenceAppGroup.setAppGroup(
+              "group.info.shiosyakeyakini.miria");
+          return await SharedPreferenceAppGroup.get("account_settings")
+              as String?;
+        }),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text(snapshot.requireData ?? "no data");
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}\n${snapshot.stackTrace}");
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
