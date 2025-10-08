dc7d7724df6db620314496e11fc67d370e363cb8
Npepperlinux <alterlinuxer@gmail.com>
2025-10-08 09:44:45 +0900

    feat: ClipListPageにクリップの残数を表示するインジケーターを追加

--- lib/view/clip_list_page/clip_list_page.dart
+++ lib/view/clip_list_page/clip_list_page.dart
@@ -26,6 +26,9 @@ class ClipListPage extends ConsumerWidget implements AutoRouteWrapper {
   Widget build(BuildContext context, WidgetRef ref) {
     ref.listen(clipsNotifierProvider, (_, _) {});
 
+    final noteEachClipsLimit =
+        accountContext.postAccount.i.policies.noteEachClipsLimit.toInt();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(S.of(context).clip),
@@ -55,7 +58,33 @@ class ClipListPage extends ConsumerWidget implements AutoRouteWrapper {
         },
         itemBuilder: (context, clip) => ClipItem(
           clip: clip,
-          trailing: _RemoveButton(id: clip.id),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  CircularProgressIndicator(
+                    value: 1 - (clip.notesCount ?? 0) / noteEachClipsLimit,
+                    color: ((clip.notesCount ?? 0) / noteEachClipsLimit) <= 0.75
+                      ? null
+                      : Colors.red,
+                  ),
+                  if ((noteEachClipsLimit - (clip.notesCount ?? 0)) <= 99)
+                    Text(
+                      (noteEachClipsLimit - (clip.notesCount ?? 0)).toString(),
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: (noteEachClipsLimit - (clip.notesCount ?? 0)) <= 0
+                          ? Colors.red
+                          : null,
+                        ),
+                    ),
+                ],
+              ),
+              _RemoveButton(id: clip.id),
+            ],
+          ),
         ),
       ),
     );
